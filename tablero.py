42f55678d1c81f2d3d908d0bddb2a12c540d89ee
EnriqueMC557 <kikemeca05@gmail.com>
2020-02-13 11:10:52 -0600

    Movimientos libres
    Se creó el método mover de la clase posiciones para lograr cambiar de posición una pieza. Este cambio de posiciones no tiene restricciones por el momento.
    Se modificó el color, contorno y tamaño de los markers para tener una mejor visualización de estos.

--- tablero.py
+++ tablero.py
@@ -6,23 +6,35 @@ Aguilar C. M. R., Mena C. E.
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-class Posiciones:
+class Posiciones():
     def __init__(self):
-        self.wh = np.array([ [1,2], [2,2], [3,2], [4,2],
+        self.bk = np.array([ [1,2], [2,2], [3,2], [4,2],
                              [5,2], [6,2], [7,2], [8,2],
                              [1,1], [8,1], [2,1], [7,1],
-                             [3,1], [6,1], [4,1], [5,1] ])
-        self.bk = np.array([ [1,7], [2,7], [3,7], [4,7],
+                             [3,1], [6,1], [5,1], [4,1] ])
+        self.wh = np.array([ [1,7], [2,7], [3,7], [4,7],
                              [5,7], [6,7], [7,7], [8,7],
                              [1,8], [8,8], [2,8], [7,8],
-                             [3,8], [6,8], [4,8], [5,8] ])
+                             [3,8], [6,8], [5,8], [4,8] ])
+    def mover(self,team,pos_i,pos_f):
+        if team == 'wh':
+            pos = self.wh.tolist()
+        else:
+            pos = self.bk.tolist()
+        for i in range(16):
+            if pos[i] == pos_i:
+                if team == 'wh':
+                    self.wh[i,:] = pos_f
+                else:
+                    self.bk[i,:] = pos_f
+                break
 
 class Piezas:
     def __init__(self):
-        self.wh = {'peon' : 'ok','torre' : 'sk', 'alfil' : 'dk',
+        self.bk = {'peon' : 'ok','torre' : 'sk', 'alfil' : 'dk',
                    'caballo' : '^k', 'rey' : '+k', 'reyna' : '*k'}
-        self.bk = {'peon' : 'oy','torre': 'sy', 'alfil' : 'dy',
-                   'caballo' : '^y', 'rey' : '+y', 'reyna' : '*y'}
+        self.wh = {'peon' : 'ow','torre': 'sw', 'alfil' : 'dw',
+                   'caballo' : '^w', 'rey' : '+w', 'reyna' : '*w'}
 
 class Tablero:
     def __init__(self):
@@ -40,52 +52,58 @@ class Tablero:
     
     def tablero(self):
         self.fig, self.ax = plt.subplots()
-        #self.ax.set_xlim(0,7)
-        #self.ax.set_ylim(0,7)
         self.ax.set_xticks(np.arange(8))
         self.ax.set_xticklabels(['A', 'B', 'C', 'D', 'E','F','G','H'])
         self.ax.set_yticks(np.arange(8))
-        self.ax.set_yticklabels(['8', '7', '6', '5', '4','3','2','1'])
+        self.ax.set_yticklabels(['1', '2', '3', '4', '5','6','7','8'])
         self.ax.imshow(self.fondo,cmap = 'binary',alpha=0.5)
         
         for i in self.it.wh.keys():
             if i == 'peon':
-                plt.plot(self.pos.wh[0:8,0]-1,self.pos.wh[0:8,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[0:8,0]-1,self.pos.wh[0:8,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 10)
             elif i == 'torre':
-                plt.plot(self.pos.wh[8:10,0]-1,self.pos.wh[8:10,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[8:10,0]-1,self.pos.wh[8:10,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 15)
             elif i == 'caballo':
-                plt.plot(self.pos.wh[10:12,0]-1,self.pos.wh[10:12,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[10:12,0]-1,self.pos.wh[10:12,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 15)
             elif i == 'alfil':
-                plt.plot(self.pos.wh[12:14,0]-1,self.pos.wh[12:14,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[12:14,0]-1,self.pos.wh[12:14,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 15)
             elif i == 'rey':
-                plt.plot(self.pos.wh[14,0]-1,self.pos.wh[14,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[14,0]-1,self.pos.wh[14,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 15)
             else:
-                plt.plot(self.pos.wh[15,0]-1,self.pos.wh[15,1]-1,self.it.wh[i])
+                plt.plot(self.pos.wh[15,0]-1,self.pos.wh[15,1]-1,
+                         self.it.wh[i], mec = 'black', ms = 15)
         
         for i in self.it.bk.keys():
             if i == 'peon':
-                plt.plot(self.pos.bk[0:8,0]-1,self.pos.bk[0:8,1]-1,self.it.bk[i])
+                plt.plot(self.pos.bk[0:8,0]-1,self.pos.bk[0:8,1]-1,
+                         self.it.bk[i], ms = 10)
             elif i == 'torre':
-                plt.plot(self.pos.bk[8:10,0]-1,self.pos.bk[8:10,1]-1,self.it.bk[i])
+                plt.plot(self.pos.bk[8:10,0]-1,self.pos.bk[8:10,1]-1,
+                         self.it.bk[i], ms = 15)
             elif i == 'caballo':
-                plt.plot(self.pos.bk[10:12,0]-1,self.pos.bk[10:12,1]-1,self.it.bk[i])
+                plt.plot(self.pos.bk[10:12,0]-1,self.pos.bk[10:12,1]-1,
+                         self.it.bk[i], ms = 15)
             elif i == 'alfil':
-                plt.plot(self.pos.bk[12:14,0]-1,self.pos.bk[12:14,1]-1,self.it.bk[i])
+                plt.plot(self.pos.bk[12:14,0]-1,self.pos.bk[12:14,1]-1,
+                         self.it.bk[i], ms = 15)
             elif i == 'rey':
-                plt.plot(self.pos.bk[14,0]-1,self.pos.bk[14,1]-1,self.it.bk[i])
+                plt.plot(self.pos.bk[14,0]-1,self.pos.bk[14,1]-1,
+                         self.it.bk[i], ms = 15)
             else:
-                plt.plot(self.pos.bk[15,0]-1,self.pos.bk[15,1]-1,self.it.bk[i])
-        #self.ax.plot(self.pos.wh[:,0]-1,self.pos.wh[:,1]-1, '^r')
-        #self.ax.plot(self.pos.bk [:,0]-1,self.pos.bk [:,1]-1, '^b')
-        #self.ax.grid(True,color='k')
-        #self.ax.set_aspect('equal')
+                plt.plot(self.pos.bk[15,0]-1,self.pos.bk[15,1]-1,
+                         self.it.bk[i], ms = 15)
         return self.ax
     
     def mostrar(self):
         self.tablero() 
-        # tab.plot(x0-0.5,y0-0.5, '^r')
-        # tab.grid(True)
         plt.show()
+
+
 
 
 
